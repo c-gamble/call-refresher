68c14d78e25437375fc2b7d11c6705a80e15e94b
Cooper Gamble <gamble.cooper23@gmail.com>
2022-10-23 17:10:30 -0500

    test

--- caller.js
+++ caller.js
@@ -6,6 +6,9 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
+client.calls.create({url: 'http://demo.twilio.com/docs/voice.xml', from: '+13464895614', to: '+15072990714'})
+.then(call => console.log(call.sid))
+.catch(error => console.log(error));
 
 const call = new CronJob(
     '* * */10 * *', 
